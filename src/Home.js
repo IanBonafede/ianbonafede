bff81b621ff7e0daa853535c7cdc8f8f24658a48
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-07-22 14:24:24 -0700

    making necessary foundation

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from './logo.svg';
 
 class Home extends React.Component {
     constructor() {
@@ -8,7 +9,22 @@ class Home extends React.Component {
 
     render() {
         return (
-                <h1>Homepage</h1>
+            <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+            </div>
         );
     }
 }
