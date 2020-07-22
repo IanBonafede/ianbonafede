248aeb2e55e57871a35bdf1668ed5b6e30e3d8f6
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-07-22 12:15:58 -0700

    Updates

---
+++ precache-manifest.f26a81cea50000feeed85cc2540ffb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d45397b3bfa86c4a21478b148ee477",
+    "url": "/ianbonafede/index.html"
+  },
+  {
+    "revision": "c7f1b58cf68674dad023",
+    "url": "/ianbonafede/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bac36a580e512b8c65d",
+    "url": "/ianbonafede/static/js/2.f471d725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ianbonafede/static/js/2.f471d725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f1b58cf68674dad023",
+    "url": "/ianbonafede/static/js/main.12159d4a.chunk.js"
+  },
+  {
+    "revision": "dcaf627da653e890ffbc",
+    "url": "/ianbonafede/static/js/runtime-main.b803e77a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ianbonafede/static/media/logo.5d5d9eef.svg"
+  }
+]);
