64ecfcb71834ac1d4cedf264910860bb950c3a14
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-07-27 15:17:58 -0700

    added logo

--- src/Home.js
+++ src/Home.js
@@ -5,26 +5,15 @@ class Home extends React.Component {
     constructor() {
         super()
         this.state = {}
+
+        //this.handleClick = this.handleClick.bind(this)
     }
+
+    //handleClick(event) {}
 
     render() {
         return (
-            <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-            </header>
-            </div>
+            <h1>Home</h1>
         );
     }
 }
