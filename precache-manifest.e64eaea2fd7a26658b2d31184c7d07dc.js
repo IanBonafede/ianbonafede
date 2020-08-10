cbe6347052b1be76d0d25f1d6a340c525d024fbf
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-08-10 15:54:20 -0700

    Updates

---
+++ precache-manifest.e64eaea2fd7a26658b2d31184c7d07dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d96218f74d9714d650594ab2c1fde4",
+    "url": "/ianbonafede/index.html"
+  },
+  {
+    "revision": "8b14d7e924f9a890d25a",
+    "url": "/ianbonafede/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1231dc016e46df776877",
+    "url": "/ianbonafede/static/js/2.1c63e2f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ianbonafede/static/js/2.1c63e2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b14d7e924f9a890d25a",
+    "url": "/ianbonafede/static/js/main.ed346528.chunk.js"
+  },
+  {
+    "revision": "dcaf627da653e890ffbc",
+    "url": "/ianbonafede/static/js/runtime-main.b803e77a.js"
+  },
+  {
+    "revision": "e825587941cbb3fd56b4cd2b1172387a",
+    "url": "/ianbonafede/static/media/SCRIPTBL.e8255879.TTF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ianbonafede/precache-manifest.5021321c14a746eee3f5c145bd262e4b.js"
+  "/ianbonafede/precache-manifest.e64eaea2fd7a26658b2d31184c7d07dc.js"
 );
 
 self.addEventListener('message', (event) => {
