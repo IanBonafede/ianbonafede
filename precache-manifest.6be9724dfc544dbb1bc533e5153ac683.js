1a9590471b4f9f5ca194132393687213684719a5
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-08-13 13:09:25 -0700

    Updates

---
+++ precache-manifest.6be9724dfc544dbb1bc533e5153ac683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab14b9a997362f09d012ece972a3506",
+    "url": "/ianbonafede/index.html"
+  },
+  {
+    "revision": "672ffc0abcc0211dee10",
+    "url": "/ianbonafede/static/css/main.1382bd12.chunk.css"
+  },
+  {
+    "revision": "4d88513b93f56b8234a9",
+    "url": "/ianbonafede/static/js/2.090b786a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ianbonafede/static/js/2.090b786a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672ffc0abcc0211dee10",
+    "url": "/ianbonafede/static/js/main.b711cec4.chunk.js"
+  },
+  {
+    "revision": "dcaf627da653e890ffbc",
+    "url": "/ianbonafede/static/js/runtime-main.b803e77a.js"
+  }
+]);
