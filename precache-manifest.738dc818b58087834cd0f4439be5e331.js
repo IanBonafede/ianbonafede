d3bc6b9eb0ed66c9ce60cb2501317c94b01dd721
IanBonafede <32549590+IanBonafede@users.noreply.github.com>
2020-08-13 16:50:54 -0700

    Updates

--- precache-manifest.738dc818b58087834cd0f4439be5e331.js
+++ precache-manifest.738dc818b58087834cd0f4439be5e331.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b755247895a5d5a91963c785df2d3676",
+    "revision": "a6f2838056dae21fb584fbd5b42febc7",
     "url": "/ianbonafede/index.html"
   },
   {
-    "revision": "8d9a3591723ca2989739",
+    "revision": "bc3cbac747ed8ab16c2e",
     "url": "/ianbonafede/static/css/main.1382bd12.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ianbonafede/static/js/2.3424f1a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8d9a3591723ca2989739",
-    "url": "/ianbonafede/static/js/main.9489c194.chunk.js"
+    "revision": "bc3cbac747ed8ab16c2e",
+    "url": "/ianbonafede/static/js/main.8c79900e.chunk.js"
   },
   {
     "revision": "dcaf627da653e890ffbc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ianbonafede/precache-manifest.a7c1182065279133491b66f53d02a977.js"
+  "/ianbonafede/precache-manifest.738dc818b58087834cd0f4439be5e331.js"
 );
 
 self.addEventListener('message', (event) => {
